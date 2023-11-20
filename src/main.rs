fb2310f5df2ca88118b631859009dfc55ec6f28d
Felix Rath <felixm.rath@gmail.com>
2023-11-20 12:05:31 +0700

    add bevy_inspector plugin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(WorldInspectorPlugin::new())
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
