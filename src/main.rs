4093fb5e00b0c5b4372eadab2022d972bc46d640
Felix Rath <felixm.rath@gmail.com>
2023-11-27 17:22:42 +0700

    Player card

--- src/main.rs
+++ src/main.rs
@@ -15,30 +15,55 @@ struct Enemy;
 
 #[derive(Debug, Component)]
 struct FightBoard {
-    fight: Entity,
+    _fight: Entity,
 }
+
+#[derive(Debug, Component)]
+struct PlayerCharacterCard {
+    _player_character: Entity,
+}
+
+const FIGHT_BOARD_SIZE: Vec2 = Vec2::new(850., 700.);
 
 // detect when `Fight` is added to the world
 fn fight_added(
     mut commands: Commands,
-    query: Query<Entity, Added<Fight>>,
+    new_fights: Query<(Entity, &Fight), Added<Fight>>,
+    player_characters: Query<&PlayerCharacter>,
+    enemies: Query<&Enemy>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    for e in query.iter() {
+    for (e, fight) in new_fights.iter() {
         println!("Detected new `Fight`, spawning `FightBoard`");
 
-        commands.spawn((
-            FightBoard { fight: e },
-            MaterialMesh2dBundle {
-                mesh: meshes
-                    .add(shape::Quad::new(Vec2::new(850., 700.)).into())
-                    .into(),
-                material: materials.add(ColorMaterial::from(Color::GRAY)),
-                transform: Transform::from_translation(Vec3::new(200., 0., 0.)),
-                ..default()
-            },
-        ));
+        commands
+            .spawn((
+                FightBoard { _fight: e },
+                MaterialMesh2dBundle {
+                    mesh: meshes.add(shape::Quad::new(FIGHT_BOARD_SIZE).into()).into(),
+                    material: materials.add(ColorMaterial::from(Color::GRAY)),
+                    transform: Transform::from_translation(Vec3::new(200., 0., -100.)),
+                    ..default()
+                },
+            ))
+            .with_children(|parent| {
+                if let Ok(_pc) = player_characters.get(fight.player_character) {
+                    parent.spawn((
+                        PlayerCharacterCard {
+                            _player_character: fight.player_character,
+                        },
+                        MaterialMesh2dBundle {
+                            mesh: meshes
+                                .add(shape::Quad::new(Vec2::new(350., 150.)).into())
+                                .into(),
+                            material: materials.add(ColorMaterial::from(Color::BLUE)),
+                            transform: Transform::from_translation(Vec3::new(-225., 225., 100.)),
+                            ..default()
+                        },
+                    ));
+                }
+            });
     }
 }
 
