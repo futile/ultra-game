55748cb718ccff1be0b29587b455a579ae07a7fe
Felix Rath <felixm.rath@gmail.com>
2023-11-27 16:44:01 +0700

    first background for fight yay!! :tada:

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 #[derive(Debug, Clone, Component)]
@@ -13,24 +13,44 @@ struct PlayerCharacter;
 #[derive(Debug, Component)]
 struct Enemy;
 
+#[derive(Debug, Component)]
+struct FightBoard {
+    fight: Entity,
+}
+
 // detect when `Fight` is added to the world
 fn fight_added(
-    query: Query<
-        // components
-        (Entity, &Fight),
-        Or<(Added<Fight>, Changed<Fight>)>,
-    >,
+    mut commands: Commands,
+    query: Query<Entity, Added<Fight>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    // for (health, xp) in query.iter() {
-    //     eprintln!("hp: {}+{}, xp: {}", health.hp, health.extra, xp.0);
-    // }
+    for e in query.iter() {
+        println!("Detected new `Fight`, spawning `FightBoard`");
 
-    for (_, fight) in query.iter() {
-        eprintln!("Fight added: {:?}", fight);
+        commands.spawn((
+            FightBoard { fight: e },
+            MaterialMesh2dBundle {
+                mesh: meshes
+                    .add(shape::Quad::new(Vec2::new(850., 700.)).into())
+                    .into(),
+                material: materials.add(ColorMaterial::from(Color::GRAY)),
+                transform: Transform::from_translation(Vec3::new(200., 0., 0.)),
+                ..default()
+            },
+        ));
+    }
+}
+
+fn fight_changed(query: Query<Entity, Changed<Fight>>) {
+    for _ in query.iter() {
+        println!("Detected changed `Fight`");
     }
 }
 
 fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
     let player_character = commands
         .spawn((PlayerCharacter, Name::new("Player Character")))
         .id();
@@ -50,6 +70,7 @@ fn main() {
         .add_plugins(WorldInspectorPlugin::new())
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Update, fight_added)
+        .add_systems(Update, fight_changed)
         .add_systems(Startup, setup)
         .run();
 }
