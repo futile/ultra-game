0062b6b09c2d65d73c4d465da6d455c50778e127
Felix Rath <felixm.rath@gmail.com>
2023-11-30 17:07:18 +0700

    refactorx

--- src/fight_board_plugin/ability_slots_section.rs
+++ src/fight_board_plugin/ability_slots_section.rs
@@ -41,55 +41,53 @@ pub fn sync_to_models(
             });
         }
 
-        let Ok(model_slots) = slots.get(section.model) else {
-            continue;
-        };
+        if let Ok(model_slots) = slots.get(section.model) {
+            let model_changed = model_slots.is_changed();
+            let section_changed = section.is_changed() && (!section.is_added());
+            let should_despawn = section_changed || model_changed;
 
-        let model_changed = model_slots.is_changed();
-        let section_changed = section.is_changed() && (!section.is_added());
-        let should_despawn = section_changed || model_changed;
+            if should_despawn {
+                commands.entity(en).clear_children();
+            }
 
-        if should_despawn {
-            commands.entity(en).clear_children();
-        }
+            let should_spawn = should_despawn || section.is_added();
 
-        let should_spawn = should_despawn || section.is_added();
-
-        if should_spawn {
-            commands.entity(en).with_children(|parent| {
-                parent.spawn(Text2dBundle {
-                    text: Text::from_section(
-                        "Ability Slots",
-                        TextStyle {
-                            font_size: 27.0,
-                            color: Color::BLACK,
-                            ..default()
-                        },
-                    )
-                    .with_alignment(TextAlignment::Left),
-                    transform: Transform::from_translation(Vec3::new(-10., 0., 1.)),
-                    ..default()
-                });
-
-                for (idx, slot) in model_slots.0.iter().enumerate() {
-                    let color = match slot.tpe {
-                        AbilitySlotType::WeaponAttack => Color::LIME_GREEN,
-                        AbilitySlotType::ShieldDefend => Color::PINK,
-                    };
-                    parent.spawn(MaterialMesh2dBundle {
-                        mesh: meshes
-                            .add(shape::Quad::new(Vec2::new(35., 35.)).into())
-                            .into(),
-                        material: materials.add(ColorMaterial::from(color)),
-                        transform: Transform::from_translation(Vec3::new(
-                            -85. + 50. * (idx as f32),
-                            -50.,
-                            1.,
-                        )),
+            if should_spawn {
+                commands.entity(en).with_children(|parent| {
+                    parent.spawn(Text2dBundle {
+                        text: Text::from_section(
+                            "Ability Slots",
+                            TextStyle {
+                                font_size: 27.0,
+                                color: Color::BLACK,
+                                ..default()
+                            },
+                        )
+                        .with_alignment(TextAlignment::Left),
+                        transform: Transform::from_translation(Vec3::new(-10., 0., 1.)),
                         ..default()
                     });
-                }
-            });
-        }
+
+                    for (idx, slot) in model_slots.0.iter().enumerate() {
+                        let color = match slot.tpe {
+                            AbilitySlotType::WeaponAttack => Color::LIME_GREEN,
+                            AbilitySlotType::ShieldDefend => Color::PINK,
+                        };
+                        parent.spawn(MaterialMesh2dBundle {
+                            mesh: meshes
+                                .add(shape::Quad::new(Vec2::new(35., 35.)).into())
+                                .into(),
+                            material: materials.add(ColorMaterial::from(color)),
+                            transform: Transform::from_translation(Vec3::new(
+                                -85. + 50. * (idx as f32),
+                                -50.,
+                                1.,
+                            )),
+                            ..default()
+                        });
+                    }
+                });
+            }
+        };
     }
 }
