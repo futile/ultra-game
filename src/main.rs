9562ab4ffceef8132fc1802cc2fa987b7e72ac67
Felix Rath <felixm.rath@gmail.com>
2023-12-23 23:47:26 +0100

    todo for abilities section

--- src/main.rs
+++ src/main.rs
@@ -52,48 +52,12 @@ fn setup(mut commands: Commands) {
     ));
 }
 
-fn ui_ability_slots(ui: &mut Ui, slots: &AbilitySlots) {
-    // TODO: add colors (again) at some point (if it fits..)
-    // old colors for reference:
-    // AbilitySlotType::WeaponAttack => Color::LIME_GREEN,
-    // AbilitySlotType::ShieldDefend => Color::PINK,
-
-    ui.heading("Ability Slots");
-    ui.indent(ui.id().with("ability_slots"), |ui: &mut Ui| {
-        for slot in slots.0.iter() {
-            ui.label(match slot.tpe {
-                AbilitySlotType::WeaponAttack => "Weapon Attack",
-                AbilitySlotType::ShieldDefend => "Shield Defend",
-            });
-        }
-    });
-}
-
-fn ui_fight_column(
-    ui: &mut Ui,
-    e: Entity,
-    names: &Query<&Name>,
-    ability_slots: &Query<&AbilitySlots>,
-) {
-    ui.indent(ui.id().with("entity_name"), |ui: &mut Ui| {
-        if let Some(name) = names.get(e).ok() {
-            ui.label(name.as_str());
-        } else {
-            ui.label("<No Name>");
-        }
-    });
-
-    if let Some(slots) = ability_slots.get(e).ok() {
-        ui.add_space(10.);
-        ui_ability_slots(ui, slots);
-    }
-}
-
-fn ui_example_system(
+fn ui_fight_windows(
     _commands: Commands,
     fights: Query<(Entity, &Fight)>,
     names: Query<&Name>,
-    slots: Query<&AbilitySlots>,
+    ability_slots: Query<&AbilitySlots>,
+    has_abilities: Query<&HasAbilities>,
     mut contexts: EguiContexts,
 ) {
     // context for the primary (so far, only) window
@@ -108,13 +72,77 @@ fn ui_example_system(
             .show(ui_ctx, |ui: &mut Ui| {
                 ui.columns(2, |columns: &mut [Ui]| {
                     columns[0].label(RichText::new("Player").heading().strong());
-                    ui_fight_column(&mut columns[0], fight.player_character, &names, &slots);
+                    ui_fight_column(
+                        &mut columns[0],
+                        fight.player_character,
+                        &names,
+                        &ability_slots,
+                        &has_abilities,
+                    );
 
                     columns[1].label(RichText::new("Enemy").heading().strong());
-                    ui_fight_column(&mut columns[1], fight.enemy, &names, &slots);
+                    ui_fight_column(
+                        &mut columns[1],
+                        fight.enemy,
+                        &names,
+                        &ability_slots,
+                        &has_abilities,
+                    );
                 });
             });
     }
+}
+
+fn ui_fight_column(
+    ui: &mut Ui,
+    e: Entity,
+    names: &Query<&Name>,
+    ability_slots: &Query<&AbilitySlots>,
+    has_abilities: &Query<&HasAbilities>,
+) {
+    ui.indent(ui.id().with("entity_name"), |ui: &mut Ui| {
+        if let Some(name) = names.get(e).ok() {
+            ui.label(name.as_str());
+        } else {
+            ui.label("<No Name>");
+        }
+    });
+
+    if let Some(slots) = ability_slots.get(e).ok() {
+        ui.add_space(10.);
+        ui_ability_slots(ui, slots);
+    }
+
+    if let Some(abilities) = has_abilities.get(e).ok() {
+        ui.add_space(10.);
+        ui_abilities(ui, abilities);
+    }
+}
+
+fn ui_ability_slots(ui: &mut Ui, slots: &AbilitySlots) {
+    // TODO: add colors (again) at some point (if it fits..)
+    // old colors for reference:
+    // AbilitySlotType::WeaponAttack => Color::LIME_GREEN,
+    // AbilitySlotType::ShieldDefend => Color::PINK,
+
+    ui.heading("Ability Slots");
+
+    ui.indent(ui.id().with("ability_slots"), |ui: &mut Ui| {
+        for slot in slots.0.iter() {
+            ui.label(match slot.tpe {
+                AbilitySlotType::WeaponAttack => "Weapon Attack",
+                AbilitySlotType::ShieldDefend => "Shield Defend",
+            });
+        }
+    });
+}
+
+fn ui_abilities(ui: &mut Ui, _abilities: &HasAbilities) {
+    ui.heading("Abilities");
+
+    ui.indent(ui.id().with("abilities"), |ui: &mut Ui| {
+        ui.label("TODO: render abilities here");
+    });
 }
 
 fn main() {
@@ -125,6 +153,6 @@ fn main() {
         .add_plugins(CoreLogicPlugin)
         .add_systems(Startup, setup)
         .add_systems(Update, bevy::window::close_on_esc)
-        .add_systems(Update, ui_example_system)
+        .add_systems(Update, ui_fight_windows)
         .run();
 }
