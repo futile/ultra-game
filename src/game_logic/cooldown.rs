c3696a768b3d51f06b772dae2be995abec08830f
Felix Rath <felixm.rath@gmail.com>
2025-08-31 22:14:53 +0200

    first impl of Cooldown stuff, but untested/unused

--- src/game_logic/cooldown.rs
+++ src/game_logic/cooldown.rs
@@ -1,8 +1,14 @@
+use std::time::Duration;
+
 use bevy::{ecs::system::SystemParam, prelude::*};
 
 use crate::{
     PerUpdateSet,
-    game_logic::{ability::AbilityId, ability_slots::AbilitySlot},
+    game_logic::{
+        ability::AbilityId,
+        ability_slots::AbilitySlot,
+        fight::{FightInterface, FightTime},
+    },
     utils::holds_held::Held,
 };
 
@@ -11,23 +17,45 @@ pub struct Cooldown {
     cooldown_timer: Timer,
 }
 
+impl Cooldown {
+    /// Starts a new [`Cooldown`] that should be on the same entity as an [`AbilityId`] or
+    /// [`AbilitySlot`]
+    pub fn new(cooldown_duration: Duration) -> Cooldown {
+        Cooldown {
+            cooldown_timer: Timer::new(cooldown_duration, TimerMode::Once),
+        }
+    }
+
+    /// Returns the duration for which this [`Cooldown`] is still running
+    pub fn remaining_cooldown(&self) -> Duration {
+        self.cooldown_timer.remaining()
+    }
+}
+
 fn tick_cooldowns(
-    cooldowns: Query<(Entity, &mut Cooldown)>,
+    cooldowns: Query<(Entity, &mut Cooldown), Without<FightTime>>,
     time: Res<Time>,
+    cooldown_interface: CooldownInterface,
+    fight_interface: FightInterface,
     mut commands: Commands,
 ) {
     let delta = time.delta();
 
     for (e, mut cooldown) in cooldowns {
-        // TODO: retrieve fight of `e` and check if it's paused -> if so, skip
+        let character = cooldown_interface.find_character_of_cooldown(e).unwrap();
+        let fight_e = fight_interface.get_fight_of_entity(character);
+
+        if fight_interface.is_fight_paused(fight_e) {
+            continue;
+        }
 
         if cooldown.cooldown_timer.finished() {
             warn!(
                 "finished Cooldown exists on entity '{e}', but should already be removed. removing it now."
             );
-        } else {
-            cooldown.cooldown_timer.tick(delta);
         }
+
+        cooldown.cooldown_timer.tick(delta);
 
         if cooldown.cooldown_timer.finished() {
             commands.entity(e).remove::<Cooldown>();
@@ -37,28 +65,19 @@ fn tick_cooldowns(
 
 #[derive(SystemParam)]
 pub struct CooldownInterface<'w, 's> {
-    cooldowns: Query<'w, 's, (Entity, &'static Cooldown)>,
-    held_ability_ids: Query<'w, 's, (Entity, &'static Held<AbilityId>)>,
-    held_ability_slots: Query<'w, 's, (Entity, &'static Held<AbilitySlot>)>,
-    commands: Commands<'w, 's>,
+    held_ability_ids: Query<'w, 's, &'static Held<AbilityId>>,
+    held_ability_slots: Query<'w, 's, &'static Held<AbilitySlot>>,
 }
 
 impl<'w, 's> CooldownInterface<'w, 's> {
-    // TODO: start_cooldown(&mut self, entity, Duration(Cooldown?), (replace_existing? maybe later,
-    // when needed))
-
-    // TODO: is_on_cooldown(&self, entity) -> bool
-
-    // TODO: remaining_cooldown(&self, entity) -> Option<Duration>
-
-    // TODO: find_character_of_cooldown(&self, entity) -> Option<Entity>
-    // will traverse `Held<AbilityId>` or `Held<AbilitySlot>` upwards
-
-    // maybe, but maybe callers could/should just use FightInterface for this step,
-    // makes everything a bit more transparent too
-    // TODO: find_fight_of_cooldown(&self, entity) -> Option<Entity>
-    // will traverse (`Held<AbilityId>` or `Held<AbilitySlot>`) then (Parent)
-    // (i.e., from AbilitySlot -> "holder")
+    /// `entity` should have a [`Cooldown`] and be either an [`AbilityId`] or [`AbilitySlot`]
+    pub fn find_character_of_cooldown(&self, entity: Entity) -> Option<Entity> {
+        // First try to find if this entity has Held<AbilityId>,
+        // then try to find if this entity has Held<AbilitySlot>
+        self.held_ability_ids
+            .related(entity)
+            .or(self.held_ability_slots.related(entity))
+    }
 }
 
 #[derive(Debug)]
@@ -66,8 +85,6 @@ pub struct CooldownPlugin;
 
 impl Plugin for CooldownPlugin {
     fn build(&self, app: &mut App) {
-        // examples:
-        // app.add_systems(Startup, add_to_ability_catalog)
         app.register_type::<Cooldown>().add_systems(
             FixedUpdate,
             tick_cooldowns.in_set(PerUpdateSet::LogicUpdate),
